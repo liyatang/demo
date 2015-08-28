88fbac803dc5609578ee44967ff96ae98dcb33cd
liyatang <liyatang@xiaonongnv.com>
2015-08-28 15:30:33 +0800

    c

---
+++ react/src/test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by liyatang on 15/8/1.
+ */
+var Test = React.createClass({
+    render: function(){
+        return <div>test</div>;
+    }
+});
+
+React.render(<Test />, document.getElementById('testContainer'));
