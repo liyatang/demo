f51c05e2554bf194b38a1bd246881d20cfd818f2
liyatang <liyatang@xiaonongnv.com>
2015-10-06 02:50:03 +0800

    c

--- react/redux-todo.js
+++ react/redux-todo.js
@@ -1,70 +1,71 @@
-let React = window.React;
-let Redux = window.Redux;
-let bindActionCreators = Redux.bindActionCreators;
-let Provider = ReactRedux.Provider;
-let connect = ReactRedux.connect;
+{
+    let React = window.React;
+    let Redux = window.Redux;
+    let bindActionCreators = Redux.bindActionCreators;
+    let Provider = ReactRedux.Provider;
+    let connect = ReactRedux.connect;
 
-const initialtate = {
-    todos: []
-};
+    const initialtate = {
+        todos: []
+    };
 
-let reducer = function (state = initialtate, action = {}) {
-    switch (action.type) {
-        case 'add':
+    let reducer = function (state = initialtate, action = {}) {
+        switch (action.type) {
+            case 'add':
+                return {
+                    todos: [
+                        ...state.todos,
+                        {
+                            text: action.text
+                        }
+                    ]
+                };
+            default :
+                return state;
+        }
+    };
+
+    let Actions = {
+        addTodo: function (text) {
             return {
-                todos: [
-                    ...state.todos,
-                    {
-                        text: action.text
-                    }
-                ]
-            };
-        default :
-            return state;
-    }
-};
+                type: 'add',
+                text
+            }
+        }
+    };
 
-let Actions = {
-    addTodo: function (text) {
-        return {
-            type: 'add',
-            text
+    let store = Redux.createStore(reducer);
+
+    class TodoApp extends React.Component {
+        render() {
+
+            return (
+                <div>
+                    <button type="button" onClick={this.handleAdd.bind(this)}>add</button>
+                    <div>
+                        {this.props.todos}
+                    </div>
+                </div>
+            );
+        }
+
+        handleAdd() {
+            const {dispatch} = this.props;
+            const actions = bindActionCreators(Actions, dispatch);
+            actions.addTodo('work');
         }
     }
-};
 
-let store = Redux.createStore(reducer);
+    let WrapTodoApp = connect(function (state) {
+        return {
+            todos: state.todos
+        };
+    })(TodoApp);
 
-class Todo extends React.Component {
-    render() {
-
-        return (
-            <div>
-                <button type="button" onClick={this.handleAdd.bind(this)}>add</button>
-                <div>
-                    {this.props.todos}
-                </div>
-            </div>
-        );
-    }
-
-    handleAdd() {
-        const {dispatch} = this.props;
-        const actions = bindActionCreators(Actions, dispatch);
-        actions.addTodo('work');
-    }
+    React.render(
+        <Provider store={store}>
+            {() => <WrapTodoApp></WrapTodoApp>}
+        </Provider>,
+        document.getElementById('reduxTest')
+    );
 }
-
-let WrapApp = connect(function (state) {
-    return {
-        todos: state.todos
-    };
-})(Todo);
-
-React.render(
-    <Provider store={store}>
-        {() => <WrapApp></WrapApp>}
-    </Provider>,
-    document.getElementById('test')
-);
-
--- react/refluxjs-todo.js
+++ react/refluxjs-todo.js
@@ -1,39 +1,43 @@
-let Reflux = window.Reflux;
-
-var Actions = Reflux.createActions([
-    'addTodo'
-]);
-
-var Store = Reflux.createStore({
-    todos: [],
-    listenables: [Actions],
-    onAddTodo: function (text) {
-        this.todos.push({
-            text: text
-        });
-        this.trigger(this.todos);
-    }
-});
+{
+    let Reflux = window.Reflux;
+    let React = window.React;
 
 
-var TodoApp = React.createClass({
-    mixins: [Reflux.connect(Store, 'todos')],
-    getInitialState: function () {
-        return {todos: []};
-    },
-    render: function () {
-        return (
-            <div>
-                <button type="button" onClick={this.handleAdd.bind(this)}>add</button>
+    var Actions = Reflux.createActions([
+        'addTodo'
+    ]);
+
+    var Store = Reflux.createStore({
+        todos: [],
+        listenables: [Actions],
+        onAddTodo: function (text) {
+            this.todos.push({
+                text: text
+            });
+            this.trigger(this.todos);
+        }
+    });
+
+
+    var TodoApp = React.createClass({
+        mixins: [Reflux.connect(Store, 'todos')],
+        getInitialState: function () {
+            return {todos: []};
+        },
+        render: function () {
+            return (
                 <div>
-                    {this.state.todos}
+                    <button type="button" onClick={this.handleAdd.bind(this)}>add</button>
+                    <div>
+                        {this.state.todos}
+                    </div>
                 </div>
-            </div>
-        )
-    },
-    handleAdd: function () {
-        Actions.addTodo('work');
-    }
-});
+            )
+        },
+        handleAdd: function () {
+            Actions.addTodo('work');
+        }
+    });
 
-React.render(<TodoApp />, document.getElementById('test'));
+    React.render(<TodoApp />, document.getElementById('refluxTest'));
+}
