6f166993b4c2195aa9b93f9100bbd9066c04344e
liyatang <liyatang@xiaonongnv.com>
2016-05-01 17:47:29 +0800

    整理

--- react/action-types.js
+++
@@ -1,9 +0,0 @@
-import keyMirror from 'keymirror';
-
-module.exports = keyMirror({
-    TODO_ADD: null,
-    TODO_DELETE: null,
-    TODO_EDIT: null,
-    TODO_DONE: null,
-    TODO_ALL_DONE: null
-});
--- spa/js/aa.js
+++
@@ -1,9 +0,0 @@
-define(function (require, exports, module) {
-
-
-    return {
-        init: function () {
-            console.log('hello , i am a. asdfasfas');
-        }
-    }
-});
